06070cc255b0835dfcf48f13d8678eaf9b7dec72
troy <troywhitetw@ymail.com>
2017-11-04 02:45:39 +0800

    .html -> .php 修正

--- login_register.php
+++ login_register.php
@@ -111,13 +111,13 @@ if (mysqli_num_rows($result) > 0) {
             <input type="image" src="img/open_btn.png" name="lo" class="left-open">
             <div class="icon-area">
                 <ul>
-                    <li><a href="index.html">
+                    <li><a href="index.php">
                             <div class="index-icon"></div>
                         </a></li>
-                    <li><a href="login.html">
+                    <li><a href="login.php">
                             <div class="member-icon"></div>
                         </a></li>
-                    <li><a href="cart_1.html">
+                    <li><a href="cart_1.php">
                             <div class="cart-icon"></div>
                         </a></li>
                 </ul>
@@ -128,13 +128,13 @@ if (mysqli_num_rows($result) > 0) {
     <div class="topbar">
         <div class="top-content">
             <ul>
-                <li><a href="index.html">首頁
+                <li><a href="index.php">首頁
                         <div class="index-icon"></div>
                     </a></li>
-                <li><a href="login.html">會員登入
+                <li><a href="login.php">會員登入
                         <div class="member-icon"></div>
                     </a></li>
-                <li><a href="cart_1.html">購物車
+                <li><a href="cart_1.php">購物車
                         <div class="cart-icon"></div>
                     </a></li>
             </ul>
@@ -147,7 +147,6 @@ if (mysqli_num_rows($result) > 0) {
         </div>
     </div>
 
-
     <div class="container main">
 
         <div class="row content no-margin-rl">
@@ -156,15 +155,13 @@ if (mysqli_num_rows($result) > 0) {
                 <div class="left-move ">
 
                     <div class="logo ">
-                        <a href="index.html">
+                        <a href="index.php">
                             <img src="img/logo.jpg" style="" alt="">
                         </a>
                     </div>
 
                     <div class="menu-area">
-
                         <ul class="fullheight" style="overflow:auto;">
-
 
                             <li>
                                 <a class="sidebar-menu">
@@ -239,7 +236,6 @@ if (mysqli_num_rows($result) > 0) {
 
                                 </ul>
                             </li>
-
 
                             <li>
                                 <a class="sidebar-menu">
@@ -353,7 +349,6 @@ if (mysqli_num_rows($result) > 0) {
                                 </ul>
                             </li>
 
-
                         </ul>
                     </div>
 
@@ -363,16 +358,19 @@ if (mysqli_num_rows($result) > 0) {
             <div class="col-sm-10">
                 <div class="beard">
                     <ul>
-                        <li><a href="index.html">首頁</a></li>
+                        <li><a href="index.php">首頁</a></li>
                         <li><img src="img/process_icon.png" alt=""></li>
-                        <li><a href="">加入會員</a></li>
+                        <li><a href="login.php">會員登入</a></li>
                     </ul>
                 </div>
                 <div class="content-area">
                     <div class="content-article">
                         <div class="login-area">
+
                             <div class="login-tittle">加入會員</div>
+
                             <form action="register.php" method="post">
+
                                 <table width="100%" border="0">
                                     <tbody>
                                     <tr>
@@ -477,26 +475,26 @@ if (mysqli_num_rows($result) > 0) {
         <footer>
             <div class="foot-area">
                 <div class="foot-menu">
-                    <div class="ft-logo"><a href="index.html"><br><br><br><img src="img/logo_foot.png" alt=""></a></div>
+                    <div class="ft-logo"><a href="index.php"><br><br><br><img src="img/logo_foot.png" alt=""></a></div>
                     <div class="ft-menu-list">
                         <ul>
-                            <li><a href="ftmenu_about.html">關於我們</a></li>
+                            <li><a href="ftmenu_about.php">關於我們</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_privacy.html">隱私權條款</a></li>
+                            <li><a href="ftmenu_privacy.php">隱私權條款</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_policy.html">服務政策</a></li>
+                            <li><a href="ftmenu_policy.php">服務政策</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_refund.html">退貨需知</a></li>
+                            <li><a href="ftmenu_refund.php">退貨需知</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_goods.html">商品寄送</a></li>
+                            <li><a href="ftmenu_goods.php">商品寄送</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_supplier.html">供應商資訊</a></li>
+                            <li><a href="ftmenu_supplier.php">供應商資訊</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_process.html">購物流程說明</a></li>
+                            <li><a href="ftmenu_process.php">購物流程說明</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_warranty.html">鑑賞期說明</a></li>
+                            <li><a href="ftmenu_warranty.php">鑑賞期說明</a></li>
                             <li>│</li>
-                            <li><a href="ftmenu_service.html">客服中心</a></li>
+                            <li><a href="ftmenu_service.php">客服中心</a></li>
                         </ul>
                     </div>
                 </div>
@@ -606,4 +604,3 @@ if (mysqli_num_rows($result) > 0) {
 </script>
 </body>
 </html>
-
