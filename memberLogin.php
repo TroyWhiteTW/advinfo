aad0ccf342f4e931c0856017a92031cd590ac867
troy <changtingweiforwork@gmail.com>
2017-11-06 14:49:31 +0800

    login，待補 SESSION

--- memberLogin.php
+++ memberLogin.php
@@ -3,7 +3,187 @@ include 'db.php';
 session_start();
 ?>
 <?php
+$errorMessage = "";
+foreach ($_POST as $k => $v) {
+    $_POST[$k] = trim($_POST[$k]);
+}
+checkData($_POST, $errorMessage);
 
-    var_dump($_POST);
+if (empty($errorMessage)) {
 
-?>
+    $insertData = encodeRegisterData($_POST);
+
+    $sql = 'SELECT * FROM members WHERE account=' . $insertData['account'] .
+        ' AND ' . $insertData['password'] .
+        ' AND ' . $insertData['type'];
+
+    $rs = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($row[0] === $_POST['account']) {
+
+        //SESSION 設定
+
+        echo "登入成功，3秒後跳轉回首頁...";
+        header("Refresh:3;url=index.php");
+    } else {
+        echo "帳號或密碼錯誤，3秒後跳轉回登入頁...";
+        header("Refresh:3;url=login.php");
+    }
+} else {
+    echo "登入資料有誤:\n" . $errorMessage;
+}
+
+
+function checkData($post, &$msg)
+{
+    foreach ($post as $k => $v) {
+        switch ($k) {
+            case 'account':
+                checkEmpty($k, $msg);
+                checkSpace($k, $msg);
+                chechEmail($k, $msg);
+                break;
+            case 'password':
+                checkEmpty($k, $msg);
+                checkSpace($k, $msg);
+                chechLengthBetweenTwoValue($k, $msg, 20, 8);
+                checkOneEngAndOneNum($k, $msg);
+                break;
+            case 'type':
+                checkEmpty($k, $msg);
+                checkSpace($k, $msg);
+                checkType($k, $msg);
+                break;
+        }
+    }
+}
+
+function checkEmpty($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        $msg .= "$k 為空\n";
+    }
+}
+
+function checkSpace($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (preg_match('/\s/', $_POST[$k])) {
+        $msg .= "$k 有空白鍵\n";
+    }
+}
+
+function chechEmail($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (!preg_match('/[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,4}/', $_POST[$k])) {
+        $msg .= "$k 非電子信箱格式\n";
+    }
+}
+
+function chechLengthBetweenTwoValue($k, &$msg, $b, $s)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (!(strlen($_POST[$k]) <= $b && strlen($_POST[$k]) >= $s)) {
+        $msg .= "$k 長度不符\n";
+    }
+}
+
+function checkOneEngAndOneNum($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (!(preg_match('/[0-9]/', $_POST[$k]) && preg_match('/[a-zA-Z]/', $_POST[$k]))) {
+        $msg .= "$k 必須至少擁有一個數字及英文";
+    }
+}
+
+function checkSamePassword($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if ($_POST[$k] !== $_POST['password']) {
+        $msg .= "$k 密碼確認有誤";
+    }
+}
+
+function checkHasNum($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (preg_match('/[0-9]/', $_POST[$k])) {
+        $msg .= "$k 不得有數字";
+    }
+}
+
+function checkOnlyNum($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (preg_match('/[^0-9]/', $_POST[$k])) {
+        $msg .= "$k 只能是數字";
+    }
+}
+
+function checkLength($k, &$msg, $l)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if (strlen($_POST[$k]) !== $l) {
+        $msg .= "$k 長度不符";
+    }
+}
+
+function checkGender($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if ($_POST[$k] !== "M" && $_POST[$k] !== "F") {
+        $msg .= "$k 錯誤";
+    }
+}
+
+function checkType($k, &$msg)
+{
+    if (empty($_POST[$k])) {
+        return;
+    }
+    if ($_POST[$k] !== "1" && $_POST[$k] !== "2") {
+        $msg .= "$k 錯誤";
+    }
+}
+
+function encodeRegisterData($rawDataArray)
+{
+    $dataArray = [
+        "account" => "\"\"",
+        "password" => "\"\"",
+        "type" => "\"\"",
+    ];
+
+    foreach ($dataArray as $k => $v) {
+        if (!empty($rawDataArray[$k])) {
+            if ($k === "password") {
+                $dataArray[$k] = "\"" . md5($rawDataArray[$k]) . "\"";
+            } else {
+                $dataArray[$k] = "\"" . $rawDataArray[$k] . "\"";
+            }
+        }
+    }
+    return $dataArray;
+}
+
+exit;
