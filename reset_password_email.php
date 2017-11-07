d0b262f09168a24b93a01c03cc2b318615c38758
troy <changtingweiforwork@gmail.com>
2017-11-07 12:18:17 +0800

    連結修改

--- reset_password_email.php
+++ reset_password_email.php
@@ -21,7 +21,7 @@ if (!empty($_POST['email'])) {
     }
 
     // TODO: 聯結網域需修改!!!
-    $link = 'http://localhost/advinfo/reset_password_email.php?email=' . $_POST['email'] . '&hash_key=' . $row[1];
+    $link = 'http://advinfo.taironlife.com/reset_password_email.php?email=' . $_POST['email'] . '&hash_key=' . $row[1];
 
     require 'PHPMailer/PHPMailerAutoload.php';
 
