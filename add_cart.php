2c1fa01b78855de8727a97fe1aff55c943ff7031
troy <changtingweiforwork@gmail.com>
2017-11-09 14:01:52 +0800

    加入購物車、直接購買，中介頁邏輯重購

--- add_cart.php
+++ add_cart.php
@@ -14,19 +14,79 @@ if ($isLogin) {
         if (gettype($_SESSION['shop_cart']) !== 'array') {
             $_SESSION['shop_cart'] = [];
         }
+
+        $sql = 'SELECT stock FROM products WHERE proid=' . "\"" . $_POST['proid'] . "\"";
+        $rs = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+//        if ($rs === false) {
+//        }防止有人繞過驗證導致資料庫返回錯誤的判斷
+
         if (haveSameProduct($_POST['proid'])) {
-            $_SESSION['shop_cart'][$_POST['proid']] += $_POST['count'];
+            //重複加入
+            if ($_POST['count'] > 10) {
+
+                echo '超過單次購買數量上限';
+                $rs->close();
+                return;
+
+            } elseif ($_SESSION['shop_cart'][$_POST['proid']] + $_POST['count'] > 10) {
+
+//                $_SESSION['shop_cart'][$_POST['proid']] = 10;
+                echo '累計數量超過單次購買數量上限';
+                $rs->close();
+                return;
+
+            }
+
+            if ($_SESSION['shop_cart'][$_POST['proid']] > $row[0]) {
+
+                echo '庫存數量不足';
+                $rs->close();
+                return;
+
+            } else {
+
+                $_SESSION['shop_cart'][$_POST['proid']] += $_POST['count'];
+                echo '已加入購物車';
+                $rs->close();
+                return;
+
+            }
+
         } else {
-            $_SESSION['shop_cart'][$_POST['proid']] = $_POST['count'];
+            // 第一次加入
+            if ($_POST['count'] > 10) {
+
+                echo '超過單次購買數量上限';
+                $rs->close();
+                return;
+
+            } elseif ($_POST['count'] > $row[0]) {
+
+                echo '庫存數量不足';
+                $rs->close();
+                return;
+
+            } else {
+
+                $_SESSION['shop_cart'][$_POST['proid']] = $_POST['count'];
+                echo '已加入購物車';
+                $rs->close();
+                return;
+
+            }
+
         }
 
-        echo '已加入購物車';
     } else {
         echo '加入購物車失敗';
+        return;
     }
 
 } else {
     echo '請先登入';
+    return;
 }
 
 function haveSameProduct($id)
