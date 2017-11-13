0f13ed35e20c315790308ff3222b8d6b799842b5
troy <troywhitetw@ymail.com>
2017-11-14 01:05:11 +0800

    side bar logic

--- side_bar.php
+++ side_bar.php
@@ -56,195 +56,195 @@
 
             ?>
 
-            <hr/>
-
-            <li>
-
-                <a class="sidebar-menu">
-                    套裝組合
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別二
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-
-            </li>
-
-            <li>
-                <a class="sidebar-menu">
-                    膠囊類
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a class="sidebar-menu">
-                    萃取液
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a class="sidebar-menu">
-                    健康器材
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a class="sidebar-menu">
-                    養身食品
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a class="sidebar-menu">
-                    滴丸
-                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                </a>
-                <ul class="sidebar-sub">
-                    <li>
-                        <a class="sidebar-level2" href="#">
-                            次類別一
-                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>
-                        </a>
-                        <ul class="sidebar-sub3 ">
-                            <li>
-                                <a href="#">
-                                    第三層類別一
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    第三層類別二
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </li>
+<!--            <hr/>-->
+<!---->
+<!--            <li>-->
+<!---->
+<!--                <a class="sidebar-menu">-->
+<!--                    套裝組合-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!---->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別二-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!---->
+<!--            </li>-->
+<!---->
+<!--            <li>-->
+<!--                <a class="sidebar-menu">-->
+<!--                    膠囊類-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+<!--            </li>-->
+<!---->
+<!--            <li>-->
+<!--                <a class="sidebar-menu">-->
+<!--                    萃取液-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+<!--            </li>-->
+<!---->
+<!--            <li>-->
+<!--                <a class="sidebar-menu">-->
+<!--                    健康器材-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+<!--            </li>-->
+<!---->
+<!--            <li>-->
+<!--                <a class="sidebar-menu">-->
+<!--                    養身食品-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+<!--            </li>-->
+<!---->
+<!--            <li>-->
+<!--                <a class="sidebar-menu">-->
+<!--                    滴丸-->
+<!--                    <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                </a>-->
+<!--                <ul class="sidebar-sub">-->
+<!--                    <li>-->
+<!--                        <a class="sidebar-level2" href="#">-->
+<!--                            次類別一-->
+<!--                            <i class="fa fa-angle-right angle-right" aria-hidden="true"></i>-->
+<!--                        </a>-->
+<!--                        <ul class="sidebar-sub3 ">-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別一-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">-->
+<!--                                    第三層類別二-->
+<!--                                </a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+<!--            </li>-->
 
         </ul>
 
