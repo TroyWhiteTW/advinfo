02d5362c288bfe82d4985dc5f650746b8c629a9a
troy <troywhitetw@ymail.com>
2017-11-14 01:03:37 +0800

    side bar logic

--- side_bar.php
+++ side_bar.php
@@ -8,6 +8,56 @@
 
         <ul class="fullheight" style="overflow:auto;">
 
+            <?php
+
+            $sideBarSql = 'SELECT * FROM proclass WHERE status=1 ORDER BY sort ASC';
+            $sideRows = [];
+            $sideBarRes = mysqli_query($conn, $sideBarSql);
+            while ($row = mysqli_fetch_assoc($sideBarRes)) {
+                $sideRows[] = $row;
+            }
+            $sideRowsCount = count($sideRows);
+
+            foreach ($sideRows as $one) {
+                if ($one['parent'] == 0) {
+                    echo '<li>';
+                    echo '<a class="sidebar-menu">';
+                    echo $one['pcname'];//第一階
+                    echo '</a>';
+                    echo '<ul class="sidebar-sub">';
+
+                    foreach ($sideRows as $two) {
+                        if ($two['parent'] == $one['no'] && $one['parent'] == 0) {
+                            echo '<li>';
+                            echo '<a class="sidebar-level2" href="#">';
+                            echo $two['pcname'];//第二階
+                            echo '</a>';
+                            echo '<ul class="sidebar-sub3 ">';
+
+                            foreach ($sideRows as $three) {
+                                if ($three['parent'] == $two['no'] && $two['parent'] != 0) {
+                                    echo '<li>';
+                                    echo '<a href="#">';
+                                    echo $three['pcname'];//第三階
+                                    echo '</a>';
+                                    echo '</li>';
+                                }
+                            }
+
+                            echo '</ul>';
+                            echo '</li>';
+                        }
+                    }
+
+                    echo '</ul>';
+                    echo '</li>';
+                }
+            }
+
+            ?>
+
+            <hr/>
+
             <li>
 
                 <a class="sidebar-menu">
